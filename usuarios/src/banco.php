3c3d1087ab07c4bb68633b2954dc98920b9e41e5
nascimentosuely392-netizen <nascimentosuely392@gmail.com>
2026-03-02 15:34:48 -0300

    banco

---
+++ usuarios/src/banco.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+
+<?php
+
+$servidor =  "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "estoque";
+
+
+//conexão com bancos de dados 
+try {
+    //tentativa de conexão com banco de dados
+    //codigo...
+    //classe PDO é usada para acessar vários tipos de banco de dados usando 
+    }
+
+    
+</body>
+</html>
